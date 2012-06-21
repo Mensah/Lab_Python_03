f91901e570b1076818dfd34cdceae26161037457
Mensah Adjei <nbadjei@gmail.com>
2012-06-21 17:06:23 +0000

    Lab03

--- cypher.py
+++ cypher.py
@@ -3,5 +3,7 @@ shift_value = int(raw_input('Your shift value: '))
 encoded_phrase = ''
 
 for c in phrase:
-    encoded_phrase = c
-print encoded_phrase
+    ascii_code = ord(c) + shift_value
+    encoded_phrase = ascii_code
+    encode_phrase = encoded_phrase + c 
+    print encoded_phrase
